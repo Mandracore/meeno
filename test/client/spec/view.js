620c5cb2b10f5452ee848ef8e1acd936ca631c8d
arcandre <romain.latroy@gmail.com>
2013-07-10 22:34:56 +0200

    updating specs for emb-tag-view

--- test/client/spec/view.js
+++ test/client/spec/view.js
@@ -28,6 +28,7 @@ describe("Embedded tag view", function() {
 
 	beforeEach(function() {
 		loadFixtures('clientSideTemplates.html');
+		setFixtures(sandbox());
 		this.view = new meenoAppCli.Classes.TagRefView();
 	});
 
@@ -43,27 +44,19 @@ describe("Embedded tag view", function() {
 	});
 
 	// Describing view's behaviour if it's already locked
-	describe("if not locked", function() {
-		describe("when pressing any key", function() {
-			it("should not be altered", function() {
-				expect (false).toBe(true);
-			});
-		});
-		describe("when clicked or on pressing key back", function() {
-			it("should be destroyed", function() {
-				expect (false).toBe(true);
-			});
-			it("should be unlinked from note", function() {
-				expect (false).toBe(true);
-			});
-		});
-	});
-
-	// Describing view's behaviour if it's already locked
 	describe("if already locked", function() {
 		describe("when pressing any key but the back key with caret inside", function() {
 			it("should not be altered", function() {
-				expect (false).toBe(true);
+				$("#sandbox").append(this.view.render().$el);
+				
+				var initHtml = this.view.$el.html();
+				var keyEvent = jQuery.Event("keydown");
+				keyEvent.which = 50; // # Some key code value
+
+				this.view.$el.trigger(keyEvent);
+
+				expect (this.view.$el).toBeVisible(); // L'élément DOM de la vue doit toujours être affiché
+				expect (initHtml).toEqual(this.view.$el.html()); // Le contenu de l'élément de la vue doit rester intact
 			});
 		});
 		describe("when clicked or on pressing key back with caret inside", function() {
