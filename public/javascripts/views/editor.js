9a19392c22e7aa31897ad2169069cd62859c3ac4
Romain Latroy <romain.latroy@gmail.com>
2016-01-30 14:05:31 +0100

    Editor almost finished, only need to update objects when their model changes

--- public/javascripts/views/editor.js
+++ public/javascripts/views/editor.js
@@ -7,7 +7,10 @@ define ([
 		'temp',
 		'channel',
 		'views/editor-object',
-	], function ($, $, _, Backbone, tools, temp, channel, EditorObjectView) {
+		'models/tag',
+		'models/task',
+		'models/filter',
+	], function ($, $, _, Backbone, tools, temp, channel, EditorObjectView, Tag, Task, Filter) {
 
 		/**
 		 * This backbone view holds the body of a note editor (where the note is actually rendered)
@@ -27,7 +30,9 @@ define ([
 			events: {
 				'click .header .close'      : 'close',
 				'click .text-editor button' : 'textEditor',
-
+				'blur input'                : 'objectInputDestroy',
+				'click .content .tag'       : 'objectUnLink',
+				'click .content .task'      : 'objectUnLink',
 
 				// 'click .title'      : 'show',
 				// 'click .delete'        : 'delete',
@@ -48,13 +53,16 @@ define ([
 				//this.options = options;
 				//Backbone.View.prototype.initialize.apply(this, arguments);
 				var self = this;
-				this.children = [];
+				// this.children = [];
 
 				this.lastSave = new Date();
 
-				this.listenTo(channel, 'keyboard:tag', function () {this.newObject("tag");});
-				this.listenTo(channel, 'keyboard:task', function () {this.newObject("task");});
-				this.listenTo(channel, 'keyboard:entity', function () {this.newObject("entity");});
+				this.listenTo(channel, 'keyboard:tag', function () {this.objectInsert("tag");});
+				this.listenTo(channel, 'keyboard:task', function () {this.objectInsert("task");});
+				this.listenTo(channel, 'keyboard:entity', function () {this.objectInsert("entity");});
+				this.listenTo(channel, 'keyboard:enter', function () {this.kbEventProxy("enter");});
+				this.listenTo(channel, 'keyboard:escape', function () {this.kbEventProxy("escape");});
+
 				this.listenTo(channel, 'editors:close:' + this.model.cid , function () {this.close();});
 			},
 
@@ -248,78 +256,231 @@ define ([
 				// var $ToC = $h1.closest(".summary ul");
 			},
 
-			newObject: function (className) {
+			/**
+			 * Insert a new object (tag, task,...) into the note content. Will just insert a form into the editor at the
+			 * caret's position
+			 * 
+			 * @method objectInsert
+			 */
+			objectInsert: function (className) {
+				document.execCommand("delete", null, false); // to remove last character like ##
 
 				var $focused = $(document.activeElement); // most efficient way to retrieve currently focus element
-				if ($focused.hasClass('content')) { return; } // No action if no focus in the editor
+				if (!$focused.hasClass('content')) { return; } // No action if no focus in the editor
 
-				console.log('>>> New '+className);
-
-				var newView = new EditorObjectView({
-					note      : this.model,
-					parent    : this,
-					modelClass: className
-				});
-
-				newView.undelegateEvents();
+				var iObjectID = (new Date()).getTime();
 
 				switch (className) {
 					case "tag":
-						var hObject = '<div class="tag" contenteditable="false"><input placeholder="Name your tag here"></div>';
+						var hObject = '<div class="tag" id ="' + iObjectID +'" contenteditable="true"><input class="mousetrap" placeholder="Name your tag here"></div>';
 						break;
 					case "task":
-						var hObject = '<div class="task" contenteditable="false">'+
+						var hObject = '<div class="task" id ="' + iObjectID +'" contenteditable="true">'+
 							'<div class="fa fa-tasks"></div>'+
-							'<input placeholder="Describe here your task">'+
+							'<input class="mousetrap" placeholder="Describe here your task">'+
 						'</div>';
 						break;
 				}
 
-				var $object = $(hObject);
 
-				tools.pasteHtmlAtCaret(hObject);
+				tools.pasteHtmlAtCaret(hObject); // Insert the object where the caret stands
+				var $objectInput = this.$('#'+iObjectID+' input');
 
-				// var newHtml = $("<div></div>").append(newView.render().el).html();
-				// tools.pasteHtmlAtCaret(
-				// 	newHtml + // The tag itself with a trick to get its html back
-				// 	"<span class='void'>&nbsp;</span>" // A place to put the caret
-				// );
-				// newView.$el = $("#" + newView.options.id); // Linking the DOM to the view
-				// newView.delegateEvents(); // Binding all events
-				// newView.options.parentDOM = this.$("section.edit-content");
-				// newView.$(".body").focus(); // Focusing on input
-				// this.children.push (newView);
-				// this.save();
+				$objectInput.focus(); // Put the focus into the input so that the user can name the object
+
+				if (className == "tag") {
+					this.objectAutocompleteInit($objectInput);
+				}
 
 				return false;
 			},
 
+			/**
+			 * Will provide an autocomplete widget for the objects requiring it (like tags)
+			 * 
+			 * @method objectAutocompleteInit
+			 */
+			objectAutocompleteInit: function ($input) {
 
+				var self = this;
+				$input.autocomplete({
+					source: function (request, response) {
+						// request.term : data typed in by the user ("new yor")
+						// response : native callback that must be called with the data to suggest to the user
+						var tagFilter = new Filter.Tag ({text: request.term});
+						response (
+							temp.coll.tags.search(tagFilter).map(function (model, key, list) {
+								return {
+									label: model.get("label"),
+									value: model.cid
+								};
+							})
+						);
+					},
+					focus: function(event, ui) {
+						$input.val(ui.item.label);
+						return false; // to cancel normal behaviour
+					},
+					select: function(event, ui) {
+						var objectModel = temp.coll.tags.get(ui.item.value) // ui.item.value == model.cid
+						self.objectLink($input, objectModel, "tag");
+						return false;
+					}
+				});
+				return false;
+			},
 
 			/**
-			 * This method should update the class of #editors in the following cases :
-			 * 1. a new editor is opened
-			 * 2. a opened editor is closed
-			 * This allows to apply the correct styling to the editors' wrapper. Also, the editors's wrapper
-			 * should be automatically minimized (which means apply the class `hidden`) when it's empty
+			 * Once the object has been created or just linked to the current note,
+			 * We update the DOM to show the user that the job is done
 			 * 
-			 * @method updateEditorsClass
+			 * @method objectFreeze
 			 */
-			// updateEditorsClass: function (callback) {
-			// 	var $editors             = this.$el.parent();
-			// 	var editorsChildrenCount = $editors.children().length;
-			// 	var editorsClass         = "children-count-" + editorsChildrenCount;
+			objectFreeze: function ($input, objectModelCid) {
+				var $parent = $input.parent();
+				var sParentNewHtml = "";
+				
+				if ($parent.hasClass('task')) {
+					sParentNewHtml += '<div class="fa fa-tasks"></div>';
+				}
+				sParentNewHtml += $input.val();
 
-			// 	// Testing
-			// 	$editors.removeClass("children-count-0");
-			// 	$editors.removeClass("children-count-1");
-			// 	$editors.removeClass("children-count-2");
-			// 	$editors.removeClass("children-count-3");
-			// 	$editors.removeClass("children-count-4");
-			// 	$editors.removeClass("children-count-5");
-			// 	$editors.addClass(editorsClass, 100, "easeOutExpo", callback());
-			// },
+				$input.remove();
+				$parent.html(sParentNewHtml);
+				$parent.attr('contenteditable',false); // To prevent the user from editing the object once linked
+				$parent.attr('data-cid',objectModelCid);
+			},
 
+			/**
+			 * A proxy to handle ENTER/ESCAPE keyboard events :
+			 * 1. To cancel object creation
+			 * 2. To submit new object or to link existing object 
+			 * 
+			 * @method kbEventProxy
+			 */
+			kbEventProxy: function (event) {
+				var $focused = $(document.activeElement); // most efficient way to retrieve currently focus element
+
+				if ($focused.prop("tagName") != "INPUT") { return; } // No action if no focus in some object input
+
+				if (event == "escape") {
+				// 1. The user wants to abort object insertion
+					$focused.blur();
+				}
+				// 2. The user wants to submit the new object (creation + linking or just linking)
+				if (event == "enter") {
+					this.objectSubmit($focused);
+				}
+			},
+
+			/**
+			 * Remove the object from DOM if the user wants to cancel
+			 * 
+			 * @method objectInputDestroy
+			 */
+			objectInputDestroy: function (event) {
+				$(event.target).parent().remove();
+			},
+
+			/**
+			 * To create/link new object
+			 * 
+			 * @method objectSubmit
+			 */
+			objectSubmit: function ($focused) {
+				var self = this;
+
+				if ($focused.val().length < 2) { return; } // Do not do anything if label is too short
+
+				var className = $focused.closest('div').hasClass('tag') ? 'tag' : 'task';
+				if (className == "task") {
+				// 2.1 For tasks, always create + link
+					var objectModel = new Task ({
+						label : $focused.val(),
+					});
+					temp.coll.tasks.add(objectModel);
+					objectModel.save();
+				} else {
+				// 2.2 If we are dealing with a tag, we can link or create + link => requires to check
+					// 2.2.0 Check whether the object already exists or not
+					var objectModel = temp.coll.tags.find(function (model) {
+						return model.get('label') == $focused.val();
+					});
+
+					if (!objectModel) {
+					// 2.2.1 The model does not exist in the DB so we create it first
+						var objectModel = new Tag ({
+							label : $focused.val(),
+						});
+
+						temp.coll.tags.add(objectModel);
+						objectModel.save();
+					}
+				}
+
+				this.objectLink($focused, objectModel, className);
+			},
+
+			/**
+			 * To link an object
+			 * 
+			 * @method objectLink
+			 */
+			objectLink: function ($focused, objectModel, className) {
+
+				switch (className) {
+					case "tag":
+						var link = this.model.get('tagLinks').find(function (link) {
+							return link.get("tag") == objectModel;
+						});
+
+						if (link) { // No need to link if it's already linked
+							alert('This tag is already linked to the current note.');
+							return false;
+						}
+
+						this.model.get('tagLinks').add({ tag: objectModel });
+						break;
+					case "task":
+						this.model.get('taskLinks').add({ tag: objectModel });
+						break;
+				}
+
+				this.objectFreeze($focused, objectModel.cid);
+				this.model.save();
+				this.save();
+			},
+
+			/**
+			 * To unlink an object
+			 * 
+			 * @method objectUnLink
+			 */
+			objectUnLink: function (event) {
+				var self        = this;
+				var $object     = $(event.target);
+				var className   = $object.hasClass("task") ? "task" : "tag";
+				var objectModel = temp.coll[className+"s"].get($object.attr('data-cid'));
+				var link        = {};
+
+				switch (className) {
+					case "tag":
+						var link = this.model.get('tagLinks').find(
+							function (link) {return link.get("tag") == objectModel; }
+						);
+						this.model.get('tagLinks').remove(link);
+						break;
+					case "task":
+						var link = this.model.get('taskLinks').find(
+							function (link) {return link.get("task") == objectModel; }
+						);
+						this.model.get('taskLinks').remove(link);
+						break;
+				}
+				$object.remove();
+				this.model.save();
+				this.save();
+			},
 
 			/*
 			delegatedKill: function() {
