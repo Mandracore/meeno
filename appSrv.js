39a9a676e2d6a4bbf0bc33ffb6f553b309992ab4
Romain Latroy <romain.latroy@gmail.com>
2016-07-21 14:50:57 +0200

    Updating pre-commit hook and appSrv

--- appSrv.js
+++ appSrv.js
@@ -93,7 +93,7 @@ mas.get('/cache.manifest',function(req,res){
 	res.end([
 		'CACHE MANIFEST',
 		// The timestamp below is meant to force cache expiration
-		'#:::timestamp::: 2016/07/21 13:59:30 ',
+		'#:::timestamp::: 2016/07/21 14:50:57 ',
 		'CACHE:', // Resources to cache
 		'/font/Moon-Light.otf',
 		'/font/Dense-Regular.otf',
